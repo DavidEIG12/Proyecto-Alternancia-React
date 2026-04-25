1adfbca4d406d9ff3ee75828dc19db7cf6aa311d
DavidEIG12 <amm24gr025@student.esgerencia.com>
2026-04-25 15:15:27 +0200

    Creacion de la base del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <main className="base-shell">
+      <h1>Proyecto React Base</h1>
+      <p>Proyecto inicial preparado para comenzar los ejercicios.</p>
+    </main>
+  )
+}
+
+export default App
